76b953a3d8993f71c0828dab3285d78c41a4cf0a
ilhan <ilhangrn@hotmail.com>
2025-01-25 14:13:55 -0500

    initials

---
+++ multi_file.h
@@ -0,0 +1,6 @@
+#ifndef MULTI_FILE_H
+#define MULTI_FILE_H
+
+void print_time(void);
+
+#endif // MULTI_FILE_H
--- my_app.c
+++ my_app.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "multi_file.h"
 
 int main(void){
 	printf("hi , we do compile \n");
+	print_time();
 	return 0;
 }
